c1412ea3851108963a04b3bad14cdd68317f1a7a
Matt Smith and Scott Fraser <matt@>
2018-06-18 10:42:27 -0700

    build variables for keys

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+function Key (major, minor) {
+  this.major = [];
+  // this.minor = [];
+}
+
+Key.prototype.scales = function () {
+  return this.major;
+};
+
+
+Key.prototype.major = function() {
+  var major = [0] + keyC [3] + keyC [4]
+}
+
+Key.prototype.minor = function() {
+  var minor =  this.key.slice([5]) + this.key.slice([1]) + this.key.slice([2])
+  return minor
+}
+$(document).ready(function)(){
+
+
+var keyC = ["C","D","E","F","G","A","B"]
+var keyDb = ["Db","Eb","F","Gb","Ab","Bb","C"]
+var keyD = ["D","E","F#","G","A","B","C#"]
+var keyEb = ["Eb","F","G","Ab","Bb","C","D"]
+var keyE = ["E","F#","G#","A","B","C#","D#"]
+var keyF = ["F","G","A","Bb","C","D","E"]
+var keyF# = ["F#","G#","A#","B","C#","D#","F"]
+var keyG = ["G","A","B","C","D","E","F#"]
+var keyAb = ["Ab","Bb","C","Db","Eb","F","G"]
+var keyA = ["A","B","C#","D","E","F#","G#"]
+var keyBb = ["Bb","C","D","Eb","F","G","A"]
+var keyB = ["B","C#","D#","E","F#","G#","A#"]
+
+
+
+
+keyC [0] + keyC [3] + keyC [4]
+keyC [5] + keyC [1] + keyC [2]
+
+});
+
+// ["A","B","C","D","E","F","G"]
+//
