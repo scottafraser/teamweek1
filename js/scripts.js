5ed69fdc379bbb77b1e5b06c1185cd695618bef0
Matt Smith and Scott Fraser <matt@>
2018-06-18 14:57:02 -0700

    jazz function

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function Key (scale, major, minor) {
 }
 
 
-Key.prototype.majorChords = function() {
+Key.prototype.majorPopChords = function() {
   this.major =  this.scale.slice(0,1) + "," + this.scale.slice(3,4) + "," + this.scale.slice(4,5);
   return this.major
 }
@@ -15,44 +15,55 @@ Key.prototype.minorChords = function() {
   return this.minor
 }
 
+Key.prototype.minorScale = function() {
+  var minor = this.scale.splice(5,7)
+  var minorScale = minor + "," + this.scale
+  return minorScale
+}
+
+function jazz(x) {
+  var jazzProg = (x[1] + 'min' + "," +  x[4] + 'maj7' + "," + x[0] + 'maj');
+  return jazzProg
+}
+
 
 $(document).ready(function(){
 
 var keyC = new Key (["C","D","E","F","G","A","B"])
-keyC.majorChords();
+keyC.majorPopChords();
 keyC.minorChords();
 var keyDb = new Key (["Db","Eb","F","Gb","Ab","Bb","C"])
-keyDb.majorChords();
+keyDb.majorPopChords();
 keyDb.minorChords();
 var keyD = new Key (["D","E","F#","G","A","B","C#"])
-keyD.majorChords();
+keyD.majorPopChords();
 keyD.minorChords();
 var keyEb = new Key (["Eb","F","G","Ab","Bb","C","D"])
-keyEb.majorChords();
+keyEb.majorPopChords();
 keyEb.minorChords();
 var keyE = new Key (["E","F#","G#","A","B","C#","D#"])
-keyE.majorChords();
+keyE.majorPopChords();
 keyE.minorChords();
 var keyF = new Key (["F","G","A","Bb","C","D","E"])
-keyF.majorChords();
+keyF.majorPopChords();
 keyF.minorChords();
 var keyFsharp = new Key (["F#","G#","A#","B","C#","D#","F"])
-keyFsharp.majorChords();
+keyFsharp.majorPopChords();
 keyFsharp.minorChords();
 var keyG = new Key (["G","A","B","C","D","E","F#"])
-keyG.majorChords();
+keyG.majorPopChords();
 keyG.minorChords();
 var keyAb = new Key (["Ab","Bb","C","Db","Eb","F","G"])
-keyAb.majorChords();
+keyAb.majorPopChords();
 keyAb.minorChords();
 var keyA = new Key (["A","B","C#","D","E","F#","G#"])
-keyA.majorChords();
+keyA.majorPopChords();
 keyA.minorChords();
 var keyBb = new Key (["Bb","C","D","Eb","F","G","A"])
-keyBb.majorChords();
+keyBb.majorPopChords();
 keyBb.minorChords();
 var keyB = new Key (["B","C#","D#","E","F#","G#","A#"])
-keyB.majorChords();
+keyB.majorPopChords();
 keyB.minorChords();
 
 });
